e78e253bd3c9d4eb46f89dea1837ed09e5e9e88f
David Cox <losttime.shuffle@gmail.com>
2016-03-01 00:26:30 -0700

    Can accept repos and determine whether to add

--- server.js
+++ server.js
@@ -41,6 +41,65 @@ app.post('/exchange/github', cors(corsOptions), function (req, res) {
 	}
 });
 
+var determineOwnership = function(token, repo, callback) {
+	request.get({
+		url: 'https://api.github.com/user',
+		method: 'GET',
+		headers: {
+			'Accept': 'application/vnd.github.v3+json',
+			'Authorization': 'token ' + token,
+			'User-Agent': 'losttime/Contribute'
+		}
+	}, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			var info = JSON.parse(body);
+			var user = info.login;
+			var url = 'https://api.github.com/repos/' + repo.fullName;
+			request.get({
+				url: url, //'https://api.github.com/repos/' + repo.full_name,
+				method: 'GET',
+				headers: {
+					'Accept': 'application/vnd.github.v3+json',
+					'Authorization': 'token ' + token,
+					'User-Agent': 'losttime/Contribute'
+				}
+			}, function(error, response, body) {
+				if (!error && response.statusCode === 200) {
+					var info = JSON.parse(body);
+					if (user === info.owner.login) {
+						callback(true);
+					} else {
+						callback();
+					}
+				} else {
+					console.error('add error2', error, body);
+					callback();
+				}
+			});
+		} else {
+			console.log('add error', error, body);
+			callback();
+		}
+	});
+};
+
+app.options('/data/repo', cors(corsOptions));
+app.post('/data/repo', cors(corsOptions), function(req, res) {
+	var authHeader = req.get('Authorization');
+	if (authHeader) {
+		var token = authHeader.split(' ')[1];
+		determineOwnership(token, req.body, function(isOwner) {
+			if (isOwner) {
+				console.log('should submit');
+			} else {
+				res.status(403).send('Forbidden');
+			}
+		});
+	} else {
+		res.status(401).send('Unauthorized');
+	}
+});
+
 app.listen(config.server.port, function () {
 	console.log('Example app listening on port %d!', config.server.port);
 });
